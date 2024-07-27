fc2649abd42ee6834f4783694d93865ccecbea8d
Uzma Khan <urbazoha@gmail.com>
2024-07-28 03:37:14 +0500

    started day-01 challenge

---
+++ day-01.js
@@ -0,0 +1,24 @@
+// Variables and Data-Types
+
+// Activities/Tasks
+
+// Activity-1 : Variable Declaration
+// Task-1 : Declare a variable using var, assign it a number and log the value to the console.
+// Task-2 : Declare a variable using let, assign it a string and log the value to the console.
+
+// Activity-2 : Constant Declaration
+// Task-3 : Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+// Activity-3 : Data Types
+// Task-4 : Create variables of different data types(number, string, boolean, object, array) assign it an initial value, and log each variable type using the typeof operator.
+
+// Activity-4 : Reassigning Vaiables
+// Task-5 :  Declare a variable using let,assign it an initial valueInNumber, reassign a new value, and log both values to the console.
+
+
+// Activity-5 : Understanding Const
+// Task-6 :  Try reassigning a variable declared with const and observe the error.
+
+// FEATURE REQUEST:
+// 1- Variable Types Console Log: Write a script that declares variables of different data types and logs both the value and type of each variable to the console.
+// 2- Reassignment Demo: Create a script that demonstrates the difference in between the behavior of let and const when it comes to reassignment.
